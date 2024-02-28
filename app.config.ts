a214eb0a1619bc49f3db6dc73db14a892787101c
Andres Prieto <andresprieto2703@gmail.com>
2024-02-28 10:06:59 -0500

    Server config
    Adding plugins for nuxt, also adding app settings

---
+++ app.config.ts
@@ -0,0 +1,4 @@
+export default defineAppConfig({
+  pageTitle: 'Agenda FLISoL',
+  description: "Agenda del Festival Latinoamericano de instalación de software libre de la ciudad de Pereira"
+})
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,13 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  runtimeConfig: {
-    apiSecret: '123',
-    public: {
-      apiBase: '/api'
-    }
-  }
   devtools: { enabled: true },
   modules: [
-    'nuxt-icons',
-    '@pinia/nuxt',
     '@nuxt/image',
     '@vite-pwa/nuxt',
     '@nuxtjs/fontaine',
     '@nuxtjs/html-validator',
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    '@nuxtjs/device',
+    'nuxt-og-image'
   ]
 })
